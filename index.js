7e71dc0ea44ef01f39a9fb819ab8f3062119c234
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-03-25 22:39:44 +0530

    updated

--- index.js
+++ index.js
@@ -15,3 +15,99 @@ function reveal() {
   }
   
   window.addEventListener("scroll", reveal);
+
+
+
+window.onload = function () {
+  var span = document.createElement('span');
+
+  span.className = 'fa';
+  span.style.display = 'none';
+  document.body.insertBefore(span, document.body.firstChild);
+  
+  // alert(window.getComputedStyle(span, null).getPropertyValue('font-family'));
+    
+  document.body.removeChild(span);
+};
+
+
+
+  // var animateHTML = function() {
+  //   var elems;
+  //   var windowHeight;
+  //   function init() {
+  //       elems = document.querySelectorAll('.block_anim');
+  //       windowHeight = window.innerHeight;
+  //       addEventHandlers();
+  //       checkPosition();
+  //   };
+  //   function addEventHandlers() {
+  //       window.addEventListener('scroll', checkPosition);
+  //       window.addEventListener('resize', init);
+  //   };
+  //   function checkPosition() {
+  //       for (var i = 0; i < elems.length; i++) {
+  //       var positionFromTop = elems[i].getBoundingClientRect().top;
+  //           if (positionFromTop - windowHeight <= 0) {
+  //               elems[i].className = elems[i].className.replace(
+  //               'block_anim',
+  //               'fadeInLeftBig'
+  //               );
+  //           }
+  //       }
+  //   }
+  //   return {
+  //       init: init
+  //   };
+  //  };
+  //  animateHTML().init();
+
+
+
+
+
+   //////////////////////////////////
+  //  var animateHTML = function() {
+  //   var elems;
+  //   var windowHeight;
+  //   function init() {
+  //       elems = document.querySelectorAll('.block_animm');
+  //       windowHeight = window.innerHeight;
+  //       addEventHandlers();
+  //       checkPosition();
+  //   };
+  //   function addEventHandlers() {
+  //       window.addEventListener('scroll', checkPosition);
+  //       window.addEventListener('resize', init);
+  //   };
+  //   function checkPosition() {
+  //       for (var i = 0; i < elems.length; i++) {
+  //       var positionFromTop = elems[i].getBoundingClientRect().top;
+  //           if (positionFromTop - windowHeight <= 0) {
+  //               elems[i].className = elems[i].className.replace(
+  //               'block_animm',
+  //               'fadeInLeftBigg'
+  //               );
+  //           }
+  //       }
+  //   }
+  //   return {
+  //       init: init
+  //   };
+  //  };
+  //  animateHTML().init();
+
+
+  const tl = new TimelineMax();
+
+tl.fromTo("#type-me", 5, {
+  width: "0",
+}, {
+  width: "870px", /* same as text-description width */
+  ease:  SteppedEase.config(24)
+}, 0); 
+
+
+
+
+
